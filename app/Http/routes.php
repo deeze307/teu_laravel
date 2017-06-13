a5d18451bfcdee2193cff3a081b112c06d161d87
deeze307 <deeze.designs@gmail.com>
2017-06-13 10:45:34 -0300

    .

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 // Usar la estructura de groups con prefix para definir las rutas.
-require('Controllers/IAserver/routes.php');
+require('Controllers/IAServer/routes.php');
 require('Controllers/Email/routes.php');
 require('Controllers/TeU/routes.php');
 
